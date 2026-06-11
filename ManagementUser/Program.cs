92c1e93962a770e842455a41dd0ffe7562deb35d
Luiza Graminho <luiza.sgraminho@gmail.com>
2026-06-10 21:16:53 -0300

    feat: Adiciona Injeção de Dependência e Habilita Swagger

--- ManagementUser/Program.cs
+++ ManagementUser/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ManagementUser.Data; // O namespace onde está seu AppDbContext
+using ManagementUser.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +22,14 @@ builder.Configuration.GetConnectionString("DefaultConnection")
 )
 );
 
+// Injeção de Dependência dos Serviços
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<PerfilService>();
+
 var app = builder.Build();
+
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
